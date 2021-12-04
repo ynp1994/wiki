b53e6b31ab48e281cc70209581ed87494fcf0021
ynp2012 <ynp2012@qq.com>
2021-12-04 12:37:49 +0800

    2-5 修改启动文案

--- src/main/java/com/huawei/wiki/WikiApplication.java
+++ src/main/java/com/huawei/wiki/WikiApplication.java
@@ -1,16 +1,23 @@
 package com.huawei.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 /**
  * @author ynp2012
  */
 @SpringBootApplication
 public class WikiApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOGGER.info("启动成功！！");
+        LOGGER.info("地址：\thttp:127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
