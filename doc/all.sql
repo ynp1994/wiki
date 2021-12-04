32d871252de5d4040e2f5af987dff45bf4e108f4
ynp2012 <ynp2012@qq.com>
2021-12-04 19:53:51 +0800

    3-5 集成mybatis generator 自动生成demo代码

--- doc/all.sql
+++ doc/all.sql
@@ -6,3 +6,12 @@ create table `test` (
     `password` varchar(50) comment '密码',
     primary key (`id`)
 ) engine = innodb default charset = utf8mb4 comment = '测试'
+
+
+drop table if exists `demo`;
+
+create table `demo` (
+                        `id` bigint not null comment 'id',
+                        `username` varchar(50) comment '用户名',
+                        primary key (`id`)
+) engine = innodb default charset = utf8mb4 comment = '测试'
